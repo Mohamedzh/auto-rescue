8bee751826178b0ea7131224f3f7dfd5f9624077
Mohamed Zahran <mohamed.zahran1@live.com>
2024-06-06 09:57:47 +0300

    added google service

---
+++ app/api/nadec/route.ts
@@ -0,0 +1,7 @@
+import { connectGoogleSheet } from "@/lib/nadec-google";
+
+export async function GET(request: Request) {
+  const products = await connectGoogleSheet();
+
+  return Response.json({ products });
+}
---
+++ lib/nadec-google.ts
@@ -0,0 +1,50 @@
+import { GoogleSpreadsheet } from "google-spreadsheet";
+import { JWT } from "google-auth-library";
+
+export const connectGoogleSheet = async () => {
+  const SCOPES = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+  ];
+
+  const credentials = JSON.parse(
+    Buffer.from(process.env.SECRET!, "base64").toString()
+  );
+
+  const sheetId = process.env.SHEET as string;
+
+  const jwtFromEnv = new JWT({
+    email: credentials.client_email,
+    key: credentials.private_key,
+    scopes: SCOPES,
+  });
+
+  const doc = new GoogleSpreadsheet(sheetId, jwtFromEnv);
+
+  await doc.loadInfo();
+
+  const sheet = doc.sheetsByIndex[1];
+  const rows = await sheet.getRows();
+
+  const headers = rows[0];
+  let targetArr: any[] = [];
+  rows.slice(1).map((row) => {
+    targetArr.push(row.toObject());
+    return targetArr;
+  });
+
+  targetArr = targetArr.map((item) => {
+    return {
+      ...item,
+      checkDate:
+        item[Object.keys(item)[7]].split(",")[1].slice(1) +
+        ", " +
+        item[Object.keys(item)[7]].split(",")[2].slice(1),
+      changeDate:
+        item[Object.keys(item)[12]].split(",")[1].slice(1) +
+        ", " +
+        item[Object.keys(item)[12]].split(",")[2].slice(1),
+    };
+  });
+  return targetArr;
+};
